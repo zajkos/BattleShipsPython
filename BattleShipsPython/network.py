6ee4f38d42bce4ebbeb191ac40a6744766a50e02
Wlokek <164378514+Wlokek@users.noreply.github.com>
2026-04-09 18:26:32 +0200

    Add files via upload

---
+++ BattleShipsPython/network.py
@@ -0,0 +1,30 @@
+# network.py
+import socket
+import json
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "127.0.0.1"
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.connect()
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            # Odbieramy pierwszą wiadomość powitalną od serwera
+            print(self.client.recv(2048).decode())
+        except socket.error as e:
+            print(f"Błąd połączenia: {e}")
+
+    def send(self, data):
+        try:
+            # Wysyłamy dane w formacie JSON
+            self.client.send(str.encode(json.dumps(data)))
+            # Czekamy na odpowiedź serwera
+            response = self.client.recv(2048).decode()
+            return json.loads(response)
+        except socket.error as e:
+            print(e)
+            return None
